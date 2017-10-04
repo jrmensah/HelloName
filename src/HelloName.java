d2d27be77f8f8a35a561997005354c0e9bf5befd
jrmensah <jmensah4@student.umuc.edu>
2017-10-04 10:27:06 -0400

    Slight modification- Removed boolean method and added prompt to user

--- src/HelloName.java
+++ src/HelloName.java
@@ -6,9 +6,10 @@ public class HelloName {
     public static void main(String args[])
     {
         String name = "";
+        System.out.println("What is your name?");
         Scanner keyboard = new Scanner(System.in);
         name = keyboard.nextLine();
-        
+
         System.out.println(helloName(name));
     }
 
